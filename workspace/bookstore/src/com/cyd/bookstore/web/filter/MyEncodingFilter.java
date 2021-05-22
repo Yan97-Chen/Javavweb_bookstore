7645cbfc5419798f7a4ad595ef4d83bc324de18e
Yan97-Chen <chenyandeng97@gmail.com>
2021-05-22 17:15:26 +0800

    user can add products to shopping car, and add or delete products.

--- workspace/bookstore/src/com/cyd/bookstore/web/filter/MyEncodingFilter.java
+++ workspace/bookstore/src/com/cyd/bookstore/web/filter/MyEncodingFilter.java
@@ -2,6 +2,7 @@ package com.cyd.bookstore.web.filter;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.http.HttpResponse;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -14,6 +15,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
 
 @WebFilter("/*")//拦截请求路径
 public class MyEncodingFilter implements Filter{
@@ -26,6 +28,11 @@ public class MyEncodingFilter implements Filter{
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		
+		//解决响应乱码
+		HttpServletResponse mResponse = (HttpServletResponse) response;
+		mResponse.setHeader("content-type", "text/html;charset=utf-8");//不属于response， 强转
+		
 		//1.设置POST请求中文乱码的问题
 	
 		HttpServletRequest hsr = (HttpServletRequest)request;
